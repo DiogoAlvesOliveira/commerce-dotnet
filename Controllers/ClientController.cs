2de708c991507764624f79bb50cdd4ffa4c9e9cb
DiogoAlvesOliveira <diogo.dream89@gmail.com>
2023-04-20 15:00:53 -0300

    Starting the controller layer

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NTTData.Data;
+using NTTData.Models;
 
 namespace NTTData.Controllers;
 
@@ -10,5 +12,18 @@ namespace NTTData.Controllers;
 [Route("api/[controller]")]
 public class ClientController : ControllerBase
 {
-    
+    private readonly AppDbContext _context;
+
+    public ClientController (AppDbContext context){
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<List<Client>> findAll(){
+        var clients = _context.Clients.ToList();
+        if (clients is null){
+            return BadRequest();
+        }
+        return clients;
+    }
 }
